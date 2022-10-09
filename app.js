15d044a2d2a97ed10f0201b4685adc7169fea22a
ishraque-dev <ahamedesrak3@gmail.com>
2022-10-09 22:15:19 +0600

    user module defined

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
-
+const Product = require('./models/product');
+const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -21,13 +22,12 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 sequelize
-  .sync()
-  .then(result => {
-    // console.log(result);
+  .sync({ force: true })
+  .then((result) => {
     app.listen(3000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
